3cd4aa9784bf54ea6faa91c91785c2c4f7dc8da4
Sol Jennings <sol.jennings@myob.com>
2020-04-29 20:50:30 +1000

    Fix up project naming

--- GearBox.Tests/GearBoxTests.cs
+++ GearBox.Tests/GearBoxTests.cs
@@ -13,7 +13,7 @@ namespace TopGear
         public void ShouldInitialiseWithGearZero()
         {
             var gearbox = new GearBox();
-            var gear = gearbox.GetS();
+            var gear = gearbox.S();
             Assert.Equal(0, gear);
         }
     }
