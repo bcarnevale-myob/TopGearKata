5f22418467decd26f4f2ee134e4d6c8b94198149
Bianca Carnevale <bianca.carnevale@myob.com>
2020-05-15 12:43:56 +1000

    Week 4

--- GearBox.Tests/GearBoxTests.cs
+++ GearBox.Tests/GearBoxTests.cs
@@ -14,11 +14,14 @@ namespace GearBox.Tests
             Assert.Equal(neutral, gearbox.GetGear());
         }
 
-        [Fact]
-        public void ShiftsToFirstFromNeutralUponFirstDoIt()
+        [Theory]
+        [InlineData(int.MinValue)]
+        [InlineData(int.MaxValue)]
+        [InlineData(0)]
+        public void ShiftsFromNeutralToFirstWithAnyRpm(int rpm)
         {
             var gearbox = new GearBox();
-            gearbox.DoIt(0);
+            gearbox.DoIt(rpm);
 
             Assert.Equal(1, gearbox.GetGear());
         }
@@ -27,16 +30,17 @@ namespace GearBox.Tests
         [InlineData(1999, 1)]
         [InlineData(2000, 1)]
         [InlineData(2001, 2)]
-        public void ShouldShiftUpAtRpmGreaterThan2000(int i, int expectedGear)
+        public void ShouldShiftUpAtRpmGreaterThan2000(int rpm, int expectedGear)
         {
             var gearbox = new GearBox();
-            gearbox.DoIt(0);
-            gearbox.DoIt(i);
+            //gearbox.DoIt(0);
+            gearbox.DoIt(int.MaxValue);
+            gearbox.DoIt(rpm);
 
             Assert.Equal(expectedGear, gearbox.GetGear());
         }
 
-
+        //tests for second to third, third to fourth, etc.
 
         [Fact]
         public void HasMaximumSixGears()
@@ -54,20 +58,31 @@ namespace GearBox.Tests
             Assert.Equal(6, gearbox.GetGear());
         }
 
-        [Fact]
-        public void Experiment()
+        [Theory]
+        [InlineData(1, 2001)]
+        [InlineData(2, 2001)]
+        [InlineData(3, 2001)]
+        [InlineData(4, 2001)]
+        [InlineData(5, 2001)]
+        [InlineData(6, 2001)]
+        [InlineData(7, 2001)]
+        public void Experiment(int shiftUpGears, int rmpToTest)
         {
             var gearbox = new GearBox();
-            gearbox.DoIt(0);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
 
-            Assert.Equal(6, gearbox.GetGear());
+            ChangeUpGears(gearbox, shiftUpGears);
+            gearbox.DoIt(rmpToTest);
+            var expected = shiftUpGears + 1;
+
+            Assert.Equal(expected, gearbox.GetGear());
+        }
+
+        private void ChangeUpGears(GearBox gearbox, int numberOfGearsShifts)
+        {
+            for (var i = 0; i < numberOfGearsShifts; i++)
+            {
+                gearbox.DoIt(2001);
+            }
         }
     }
 }
