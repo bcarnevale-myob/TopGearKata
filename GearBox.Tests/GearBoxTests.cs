5c7cfabbedd156fcdd32800d11e542bddbb48a35
Bianca Carnevale <bianca.carnevale@myob.com>
2020-05-25 08:19:19 +1000

    Week Four

--- GearBox.Tests/GearBoxTests.cs
+++ GearBox.Tests/GearBoxTests.cs
@@ -30,11 +30,10 @@ namespace GearBox.Tests
         [InlineData(1999, 1)]
         [InlineData(2000, 1)]
         [InlineData(2001, 2)]
-        public void ShouldShiftUpAtRpmGreaterThan2000(int rpm, int expectedGear)
+        public void ShiftsUpAtRpmGreaterThan2000(int rpm, int expectedGear)
         {
             var gearbox = new GearBox();
-            //gearbox.DoIt(0);
-            gearbox.DoIt(int.MaxValue);
+            gearbox.DoIt(0);
             gearbox.DoIt(rpm);
 
             Assert.Equal(expectedGear, gearbox.GetGear());
@@ -47,34 +46,27 @@ namespace GearBox.Tests
         {
             var gearbox = new GearBox();
             gearbox.DoIt(0);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
-            gearbox.DoIt(2001);
 
+            ChangeUpGears(gearbox, 5);
             Assert.Equal(6, gearbox.GetGear());
+
+            ChangeUpGears(gearbox, 1);
+            Assert.NotEqual(7, gearbox.GetGear());
         }
 
-        [Theory]
-        [InlineData(1, 2001)]
-        [InlineData(2, 2001)]
-        [InlineData(3, 2001)]
-        [InlineData(4, 2001)]
-        [InlineData(5, 2001)]
-        [InlineData(6, 2001)]
-        [InlineData(7, 2001)]
-        public void Experiment(int shiftUpGears, int rmpToTest)
+        [Fact]
+        public void ShiftsUpAtRpmGreaterThan2000ForEveryGear()
         {
             var gearbox = new GearBox();
+            gearbox.DoIt(0);
 
-            ChangeUpGears(gearbox, shiftUpGears);
-            gearbox.DoIt(rmpToTest);
-            var expected = shiftUpGears + 1;
-
-            Assert.Equal(expected, gearbox.GetGear());
+            for (var gear = 1; gear <= 5; gear++)
+            {
+                gearbox.DoIt(2000);
+                Assert.Equal(gear, gearbox.GetGear());
+                gearbox.DoIt(2001);
+                Assert.Equal(gear + 1, gearbox.GetGear());
+            }
         }
 
         private void ChangeUpGears(GearBox gearbox, int numberOfGearsShifts)
