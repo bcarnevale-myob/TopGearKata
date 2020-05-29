0805de8af8710e52551da6a8e67a8143fa3ec7a3
Bianca Carnevale <bianca.carnevale@myob.com>
2020-05-29 12:01:09 +1000

    Week Five

--- GearBox.Tests/GearBoxTests.cs
+++ GearBox.Tests/GearBoxTests.cs
@@ -39,8 +39,6 @@ namespace GearBox.Tests
             Assert.Equal(expectedGear, gearbox.GetGear());
         }
 
-        //tests for second to third, third to fourth, etc.
-
         [Fact]
         public void HasMaximumSixGears()
         {
@@ -69,12 +67,63 @@ namespace GearBox.Tests
             }
         }
 
+        [Theory]
+        [InlineData(499, 1)]
+        [InlineData(500, 2)]
+        [InlineData(501, 2)]
+        public void ShiftsDownAtRpmLessThan500(int rpm, int expectedGear)
+        {
+            var gearbox = CreateGearboxInGear(2);
+
+            gearbox.DoIt(rpm);
+
+            Assert.Equal(expectedGear, gearbox.GetGear());
+        }
+
+        [Theory]
+        [InlineData(499)]
+        [InlineData(2000)]
+        [InlineData(0)]
+        [InlineData(int.MinValue)]
+        public void DoesNotShiftFromFirstToNeutral(int rpm)
+        {
+            var gearbox = CreateGearboxInGear(1);
+            gearbox.DoIt(rpm);
+
+            Assert.Equal(1, gearbox.GetGear());
+        }
+
+        [Fact]
+        public void ShiftsDownAtRpmLessThan500ForEveryGear()
+        {
+            var gearbox = CreateGearboxInGear(6);
+
+            for (var gear = 6; gear > 1; gear--)
+            {
+                gearbox.DoIt(500);
+                Assert.Equal(gear, gearbox.GetGear());
+                gearbox.DoIt(499);
+                Assert.Equal(gear - 1, gearbox.GetGear());
+            }
+        }
+
         private void ChangeUpGears(GearBox gearbox, int numberOfGearsShifts)
         {
             for (var i = 0; i < numberOfGearsShifts; i++)
             {
                 gearbox.DoIt(2001);
             }
+        }
+
+        private GearBox CreateGearboxInGear(int gear)
+        {
+            var gearbox = new GearBox();
+            for (var i = 0; i < gear; i++)
+            {
+                gearbox.DoIt(2001);
+            }
+
+            return gearbox;
         }
     }
 }
--- GearBox/GearBox.cs
+++ GearBox/GearBox.cs
@@ -2,47 +2,5 @@
 {
     public class GearBox
     {
-        private int _gear = 0;
-        private int e = 0;
 
-
-
-        public void DoIt(int i)
-        {
-
-            if (_gear < 0)
-            {
-                // do nothing!
-                e = i;
-            }
-            else
-            {
-                if (_gear > 0)
-                {
-                    if (i > 2000)
-                    {
-                        _gear++;
-                    }
-                    else if (i < 500)
-                    {
-                        _gear--;
-                    }
-                }
-            }
-
-            if (_gear > 6)
-            {
-                _gear--;
-            }
-            else if (_gear < 1)
-            {
-                _gear++;
-            }
-
-            e = i;
-        }
-
-        public int GetGear() => _gear;
-        public int E() => e;
     }
-}
