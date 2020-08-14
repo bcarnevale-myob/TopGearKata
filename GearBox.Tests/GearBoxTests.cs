d617326c6658ce175510ca7c2ed3192958199ef3
Bianca Carnevale <bianca.carnevale@myob.com>
2020-08-14 12:54:34 +1000

    Week 15

--- GearBox.Tests/GearBoxTests.cs
+++ GearBox.Tests/GearBoxTests.cs
@@ -5,15 +5,13 @@ namespace GearBox.Tests
 {
     public class GearBoxTests
     {
-        // 1. New gearbox starts in neutral
-        // 2. Gearbox shifts from neutral to first gear with any RPM
-        // 3. From first gear onwards, shifts up with RPM greater than 2000
-        // 4. Maximum gear is 6
-        // 5. Shifts down at RPM less than 500 until gear 1
-        // 6. Never shifts from gear 1 back to neutral
+
+        // LAST WEEK: seperated default and custom in test class, discussed if we understood all tests
+        // NEXT WEEK: Custom tests to be discussed, how to prevent first gear and top gear being used incorrectly
+        // checks, throw exceptions
+        // FIRST DRIVER: Adam
         const int neutral = 0;
 
-        // 1. New gearbox starts in neutral
         [Fact]
         public void StartsInNeutral()
         {
@@ -21,13 +19,12 @@ namespace GearBox.Tests
             Assert.Equal(neutral, gearbox.GetGear());
         }
 
-        // 2. Gearbox shifts from neutral to first gear with any RPM
         [Theory]
         [InlineData(int.MinValue)]
         [InlineData(int.MaxValue)]
         [InlineData(0)]
         [InlineData(500)]
-        public void ShiftsFromNeutralToFirstWithAnyRpm(int rpm)
+        public void ShiftsFromNeutralToFirstWithAnyRpmValue(int rpm)
         {
             var gearbox = new GearBox();
 
@@ -35,73 +32,8 @@ namespace GearBox.Tests
             Assert.Equal(1, gearbox.GetGear());
         }
 
-        // 3. From first gear onwards, shifts up with RPM greater than 2000
         [Theory]
-        [InlineData(1999, 1)]
-        [InlineData(2000, 1)]
-        [InlineData(2001, 2)]
-        public void ShiftsUpAtRpmGreaterThan2000(int rpm, int expectedGear)
-        {
-            GearBox gearbox = CreateGearboxInGear(1);
-            gearbox.ChangeGears(rpm);
-            Assert.Equal(expectedGear, gearbox.GetGear());
-        }
-
-        // 3. From first gear onwards, shifts up with RPM greater than 2000
-        [Fact]
-        public void ShiftsUpAtRpmGreaterThan2000ForEveryGear()
-        {
-            GearBox gearbox = CreateGearboxInGear(1);
-            for (var gear = 1; gear <= 5; gear++)
-            {
-                gearbox.ChangeGears(2000);
-                Assert.Equal(gear, gearbox.GetGear());
-                gearbox.ChangeGears(2001);
-                Assert.Equal(gear + 1, gearbox.GetGear());
-            }
-        }
-
-        // 4. Maximum gear is 6
-        [Fact]
-        public void HasDefaultMaximumSixGears()
-        {
-            GearBox gearbox = CreateGearboxInGear(1);
-            ChangeUpGears(gearbox, 5);
-            Assert.Equal(6, gearbox.GetGear());
-            ChangeUpGears(gearbox, 1);
-            Assert.NotEqual(7, gearbox.GetGear());
-        }
-
-
-        // 5. Shifts down at RPM less than 500 until gear 1
-        [Theory]
-        [InlineData(499, 1)]
-        [InlineData(500, 2)]
-        [InlineData(501, 2)]
-        public void ShiftsDownAtRpmLessThan500(int rpm, int expectedGear)
-        {
-            var gearbox = CreateGearboxInGear(2);
-            gearbox.ChangeGears(rpm);
-            Assert.Equal(expectedGear, gearbox.GetGear());
-        }
-
-        // 5. Shifts down at RPM less than 500 until gear 1
-        [Fact]
-        public void ShiftsDownAtRpmLessThan500ForEveryGear()
-        {
-            var gearbox = CreateGearboxInGear(6);
-            for (var gear = 6; gear > 1; gear--)
-            {
-                gearbox.ChangeGears(500);
-                Assert.Equal(gear, gearbox.GetGear());
-                gearbox.ChangeGears(499);
-                Assert.Equal(gear - 1, gearbox.GetGear());
-            }
-        }
-
-        // 6. Never shifts from gear 1 back to neutral
-        [Theory]
-        [InlineData(499)]
+        [InlineData(1)]
         [InlineData(0)]
         [InlineData(int.MinValue)]
         public void DoesNotShiftFromFirstToNeutral(int rpm)
@@ -110,38 +42,105 @@ namespace GearBox.Tests
             gearbox.ChangeGears(rpm);
             Assert.Equal(1, gearbox.GetGear());
         }
-
-        [Fact]
-        public void CanShiftUpWithCustomThresholds()
+        //---------------------------------------------------------------------------------------------------------
+        #region DefaultTests 
+        public class DefaultBehaviourTests
         {
-            var gears = new List<Gear>() { Gear.CreateFirst(1000), Gear.CreateTop(300) };
-            var gearbox = new GearBox(gears);
-            gearbox.ChangeGears(0); //neutral to 1
-            gearbox.ChangeGears(3001); //1 to 2
 
-            Assert.Equal(2, gearbox.GetGear());
+            [Fact]
+            public void HasMaximumSixGears()
+            {
+                GearBox gearbox = CreateGearboxInGear(1);
+                ChangeUpGears(gearbox, 5);
+                Assert.Equal(6, gearbox.GetGear());
+                ChangeUpGears(gearbox, 1);
+                Assert.NotEqual(7, gearbox.GetGear());
+            }
+
+            [Theory]
+            [InlineData(1999, 1)]
+            [InlineData(2000, 1)]
+            [InlineData(2001, 2)]
+            public void ShiftsUpAtRpmGreaterThan2000(int rpm, int expectedGear)
+            {
+                GearBox gearbox = CreateGearboxInGear(1);
+                gearbox.ChangeGears(rpm);
+                Assert.Equal(expectedGear, gearbox.GetGear());
+            }
+
+            [Fact]
+            public void ShiftsUpAtRpmGreaterThan2000ForEveryGear()
+            {
+                GearBox gearbox = CreateGearboxInGear(1);
+                for (var gear = 1; gear <= 5; gear++)
+                {
+                    gearbox.ChangeGears(2000);
+                    Assert.Equal(gear, gearbox.GetGear());
+                    gearbox.ChangeGears(2001);
+                    Assert.Equal(gear + 1, gearbox.GetGear());
+                }
+            }
+
+            [Theory]
+            [InlineData(499, 1)]
+            [InlineData(500, 2)]
+            [InlineData(501, 2)]
+            public void ShiftsDownAtRpmLessThan500(int rpm, int expectedGear)
+            {
+                var gearbox = CreateGearboxInGear(2);
+                gearbox.ChangeGears(rpm);
+                Assert.Equal(expectedGear, gearbox.GetGear());
+            }
+
+            [Fact]
+            public void ShiftsDownAtRpmLessThan500ForEveryGear()
+            {
+                var gearbox = CreateGearboxInGear(6);
+                for (var gear = 6; gear > 1; gear--)
+                {
+                    gearbox.ChangeGears(500);
+                    Assert.Equal(gear, gearbox.GetGear());
+                    gearbox.ChangeGears(499);
+                    Assert.Equal(gear - 1, gearbox.GetGear());
+                }
+            }
+
         }
-
-        [Fact]
-        public void CanHaveGearsWithDifferentThresholds()
+        #endregion
+//---------------------------------------------------------------------------------------------------------
+        public class CustomBehaviourTests
         {
-            var gear1 = Gear.CreateFirst(1000);
-            var gear2 = Gear.Create(500, 2000);
-            var gear3 = Gear.CreateTop(300);
+            [Fact]
+            public void CanShiftUpWithCustomThresholds()
+            {
+                var gears = new List<Gear>() { Gear.CreateFirst(1000), Gear.CreateTop(300) };
+                var gearbox = new GearBox(gears);
+                gearbox.ChangeGears(0); 
+                gearbox.ChangeGears(3001); 
 
-            var gears = new List<Gear>() { gear1, gear2, gear3};
-            var gearBox = new GearBox(gears);
+                Assert.Equal(2, gearbox.GetGear());
+            }
 
-            gearBox.ChangeGears(1);     //neutral to 1
-            gearBox.ChangeGears(1001);  // 1 to 2
-            gearBox.ChangeGears(1001);  // should not change
-            gearBox.ChangeGears(2001);  // 2 to 3
+            [Fact]
+            public void CanHaveGearsWithDifferentThresholds()
+            {
+                var gear1 = Gear.CreateFirst(1000);
+                var gear2 = Gear.Create(500, 2000);
+                var gear3 = Gear.CreateTop(300);
 
-            Assert.Equal(3, gearBox.GetGear());
-        }
+                var gears = new List<Gear>() { gear1, gear2, gear3};
+                var gearBox = new GearBox(gears);
 
-        // Helper method: changes up gears
-        private void ChangeUpGears(GearBox gearbox, int numberOfGearsShifts)
+                gearBox.ChangeGears(1);   
+                gearBox.ChangeGears(1001);
+                gearBox.ChangeGears(1001);  
+                gearBox.ChangeGears(2001);  
+
+                Assert.Equal(3, gearBox.GetGear());
+            }
+        }       
+
+        private static void ChangeUpGears(GearBox gearbox, int numberOfGearsShifts)
         {
             for (var i = 0; i < numberOfGearsShifts; i++)
             {
@@ -149,12 +148,7 @@ namespace GearBox.Tests
             }
         }
 
-        // LAST WEEK: factory methods
-        // NEXT WEEK: problem: createFirst, createTop can be used more than once and in the wrong place
-        // maybe: checks on gear, throw exceptions
-        // michelles turn to drive
-
-        private GearBox CreateGearboxInGear(int gear)
+        private static GearBox CreateGearboxInGear(int gear)
         {
             var gearbox = new GearBox();
             for (var i = 0; i < gear; i++)
